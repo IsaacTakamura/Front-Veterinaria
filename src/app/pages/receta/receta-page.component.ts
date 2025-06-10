ca6a12b6c203f5a8b6caef1168bc55976eb8ab5a
naptap12 <renzhinos199@gmail.com>
2025-06-09 22:40:06 -0500

    feat: implement receta form with input fields and preview display

--- src/app/pages/receta/receta-page.component.ts
+++ src/app/pages/receta/receta-page.component.ts
@@ -1,15 +1,21 @@
+
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: "app-receta-page",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: "./receta-page.component.html",
   styleUrls: ["./receta-page.component.css"],
 })
 export class RecetaPageComponent {
   title = "Sistema de Atención Veterinaria";
+  nombreMascota: string = '';
+  diagnostico: string = '';
+  doctor: string = 'Nicolas alayo arias';
+  fecha: string = new Date().toISOString().substring(0, 10);
 
   isMenuOpen = false;
   toggleMenu() {
