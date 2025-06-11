a5acaacd5c303e2a67ea70b8294303ff0839e01d
naptap12 <renzhinos199@gmail.com>
2025-06-11 13:30:02 -0500

    feat: enhance receta page with improved form structure and styling

--- src/app/pages/receta/receta-page.component.ts
+++ src/app/pages/receta/receta-page.component.ts
@@ -14,11 +14,21 @@ export class RecetaPageComponent {
   title = "Sistema de Atención Veterinaria";
   nombreMascota: string = '';
   diagnostico: string = '';
-  doctor: string = 'Nicolas alayo arias';
+  doctor: string = '';
   fecha: string = new Date().toISOString().substring(0, 10);
 
   isMenuOpen = false;
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
+
+  registrarReceta() {
+  console.log("Receta médica registrada:");
+  console.log("Mascota:", this.nombreMascota);
+  console.log("Fecha:", this.fecha);
+  console.log("Diagnóstico:", this.diagnostico);
+  console.log("Doctor:", this.doctor);
+  // Aquí podrías enviar los datos a un servicio, API, etc.
+}
+
 }
