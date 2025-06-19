175409eaa0ac2c53e37104d34af2349ad9bde3c9
Isaac-UTP <u21205269@utp.edu.pe>
2025-06-19 11:45:51 -0500

    feat: update login component selector and integrate LoginComponent for improved functionality

--- src/app/pages/login/login-page.component.ts
+++ src/app/pages/login/login-page.component.ts
@@ -2,15 +2,16 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from '../../components/vet-clinic-login/login.component';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-login-page',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, LoginComponent],
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginComponent {
+export class LoginComponentPageComponent {
   username = '';
   password = '';
 
