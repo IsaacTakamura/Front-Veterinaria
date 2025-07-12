4aed95cfd549f7c690f31c57716b629ba2a1a81d
Isaac-UTP <u21205269@utp.edu.pe>
2025-07-12 11:31:52 -0500

    Se ajustaron los iconos de la tabla para que sean mas intuitivos para las personas

--- src/app/components/enfermeria/tabla/tabla-citas.component.ts
+++ src/app/components/enfermeria/tabla/tabla-citas.component.ts
@@ -2,12 +2,13 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule, NgClass, NgIf, NgFor } from '@angular/common';
 import { EstadoCitaColorPipe } from '../../shared/pipes/estado-cita-color.pipe';
 import { CitaTabla } from '../../shared/interfaces/cita-tabla.model';
+import { IconStethoscopeComponent } from '../../icons/icon-stethoscope.component';
 
 @Component({
   selector: 'app-tabla-citas',
   templateUrl: './tabla-citas.component.html',
   standalone: true,
-  imports: [CommonModule, NgClass, NgIf, NgFor, EstadoCitaColorPipe]
+  imports: [CommonModule, NgClass, NgIf, NgFor, EstadoCitaColorPipe, IconStethoscopeComponent]
 })
 export class TablaCitasComponent {
   @Input() citas: CitaTabla[] = [];
