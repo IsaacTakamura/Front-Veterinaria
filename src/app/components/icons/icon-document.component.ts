aa86bf5ae34d53ad5e698d790936d0b7f4220cc0
Isaac-UTP <u21205269@utp.edu.pe>
2025-07-15 00:37:06 -0500

    se pusieron los iconos adecuados para que se entienda la barra lateral de asistente

--- src/app/components/icons/icon-document.component.ts
+++ src/app/components/icons/icon-document.component.ts
@@ -4,12 +4,8 @@ import { Component, Input } from '@angular/core';
   selector: 'app-icon-document',
   standalone: true,
   template: `
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" [attr.width]="size" [attr.height]="size" [attr.fill]="color" [attr.stroke]="strokeColor" [attr.stroke-width]="strokeWidth">
-      <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/>
-      <polyline points="14,2 14,8 20,8"/>
-      <line x1="16" y1="13" x2="8" y2="13"/>
-      <line x1="16" y1="17" x2="8" y2="17"/>
-      <polyline points="10,9 9,9 8,9"/>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" [attr.width]="size" [attr.height]="size" [attr.fill]="color" [attr.stroke]="strokeColor" [attr.stroke-width]="strokeWidth">
+      <path d="M0 64C0 28.7 28.7 0 64 0L224 0l0 128c0 17.7 14.3 32 32 32l128 0 0 38.6C310.1 219.5 256 287.4 256 368c0 59.1 29.1 111.3 73.7 143.3c-3.2 .5-6.4 .7-9.7 .7L64 512c-35.3 0-64-28.7-64-64L0 64zm384 64l-128 0L256 0 384 128zm48 96a144 144 0 1 1 0 288 144 144 0 1 1 0-288zm16 80c0-8.8-7.2-16-16-16s-16 7.2-16 16l0 48-48 0c-8.8 0-16 7.2-16 16s7.2 16 16 16l48 0 0 48c0 8.8 7.2 16 16 16s16-7.2 16-16l0-48 48 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-48 0 0-48z"/>
     </svg>
   `,
   styles: []
