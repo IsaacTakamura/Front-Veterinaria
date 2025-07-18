4b2c46dbc743dd7cf11a365043ed104a5f80c24b
Isaac-UTP <u21205269@utp.edu.pe>
2025-07-17 22:51:10 -0500

    se agregaron los endpoints para listar triaje por mascota id para veterinario y actualizar triaje

--- src/app/core/services/triaje.service.ts
+++ src/app/core/services/triaje.service.ts
@@ -1,14 +1,27 @@
 // core/services/triaje.service.ts
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Triaje } from '../../components/shared/interfaces/triaje.model';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class TriajeService {
   private api = '/api/v1/asistente';
+  private apiVet = '/api/v1/vet';
 
-  constructor(private http: HttpClient) { }
+  private getHeaders(): HttpHeaders {
+    const token = this.authService.getToken();
+    return new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+  }
+
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService
+  ) { }
 
   crearTriaje(triaje: Triaje): Observable<Triaje> {
     // POST /api/v1/asistente/crearTriaje
@@ -75,4 +88,58 @@ export class TriajeService {
     */
   }
 
+  //? Endpoint para veterinario
+
+  // Listar triaje para mascota por id de mascota para veterinario
+  listarTriajePorMascotaIdVeterinario(mascotaId: number): Observable<Triaje[]> {
+    // GET /api/v1/vet/triaje/mascota/{mascotaId}
+    return this.http.get<Triaje[]>(`${this.apiVet}/triaje/mascota/${mascotaId}`, {
+      headers: this.getHeaders()
+    });
+  }
+  /*
+  Se espera algo como:
+  {
+  "codigo": 0,
+  "message": "string",
+  "data": [
+    {
+      "triajeId": 0,
+      "temperatura": 0,
+      "peso": 0,
+      "frecuenciaCardiaca": 0,
+      "frecuenciaRespiratoria": 0,
+      "observaciones": "string",
+      "mascotaId": 0,
+      "fechaRegistro": "2025-07-18T03:39:25.809Z",
+      "fechaActualizacion": "2025-07-18T03:39:25.809Z"
+    }
+  ]
+}
+  */
+
+  // Actualizar triaje por id de mascota para veterinario
+  actualizarTriajeVeterinario(id: number, triaje: Triaje): Observable<Triaje> {
+    // PUT /api/v1/vet/triaje/{id}
+    return this.http.put<Triaje>(`${this.apiVet}/triaje/${id}`, triaje, {
+      headers: this.getHeaders()
+    });
+  }
+  /*
+{
+  "codigo": 0,
+  "message": "string",
+  "data": {
+    "triajeId": 0,
+    "temperatura": 0,
+    "peso": 0,
+    "frecuenciaCardiaca": 0,
+    "frecuenciaRespiratoria": 0,
+    "observaciones": "string",
+    "mascotaId": 0,
+    "fechaRegistro": "2025-07-18T03:39:25.803Z",
+    "fechaActualizacion": "2025-07-18T03:39:25.803Z"
+  }
+}
+  */
 }
